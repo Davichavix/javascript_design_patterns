a259efde3ae9693864b81e8ff311a0417d75914e
David Chan <dchan902@gmail.com>
2022-05-22 15:05:23 -0600

    add solution to singleton pattern

--- singleton_pattern.js
+++ singleton_pattern.js
@@ -17,3 +17,18 @@ class Printer {
 const obj1 = Printer.getInstance(2)
 const obj2 = Printer.getInstance(3)
 
+
+let configure = null;
+class ConfigureVals{
+  constructor(initvalues){
+      this.xpoint = initvalues.xpoint || 0;
+      this.ypoint = initvalues.ypoint || 0;
+      this.shape = initvalues.shape || null;
+  }
+  static getConfiguration(initvalues){
+      if (!configure) {
+          configure = new ConfigureVals(initvalues)
+          }
+          return configure;
+  }
+}
