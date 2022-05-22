43b083c226795051bfa74169c8668b40bb6f8ee2
David Chan <dchan902@gmail.com>
2022-05-21 21:30:51 -0600

    create chocolate using iceCreamFactory class

--- factory_pattern.js
+++ factory_pattern.js
@@ -5,6 +5,28 @@ class IceCreamFactory {
       if (flavor === 'chocolate') {
         iceCream = new Chocolate();
       }
+      else if (flavor === 'mint') {
+        iceCream = new Mint()
+      }
+      else if (flavor === 'strawberry') {
+        iceCream = new Strawberry()
+      }
+      return iceCream;
     }
   }
 }
+
+class Chocolate {
+  constructor() {
+    this.icecreamFlavor = "chocolate";
+    this.message = function() {
+      return `You chose the ${this.icecreamFlavor} flavor.`
+    }
+  }
+}
+
+const iceCreamFactory = new IceCreamFactory();
+
+const chocolate = iceCreamFactory.createIcecream('chocolate');
+
+console.log(chocolate.message())
